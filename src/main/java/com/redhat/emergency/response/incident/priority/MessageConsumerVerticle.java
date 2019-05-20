00f576ba1e0a1b248eea764a4c4a563ba2a0730a
Bernard Tison <bernard.tison@gmail.com>
2019-05-20 16:14:37 +0200

    kafka with mTLS

--- src/main/java/com/redhat/emergency/response/incident/priority/MessageConsumerVerticle.java
+++ src/main/java/com/redhat/emergency/response/incident/priority/MessageConsumerVerticle.java
@@ -10,6 +10,9 @@ import io.vertx.core.logging.LoggerFactory;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.kafka.client.consumer.KafkaConsumer;
 import io.vertx.reactivex.kafka.client.consumer.KafkaConsumerRecord;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.config.SslConfigs;
 
 public class MessageConsumerVerticle extends AbstractVerticle {
 
@@ -22,12 +25,17 @@ public class MessageConsumerVerticle extends AbstractVerticle {
 
         return Completable.fromMaybe(vertx.rxExecuteBlocking(future -> {
             Map<String, String> kafkaConfig = new HashMap<>();
-            kafkaConfig.put("bootstrap.servers", config().getString("bootstrap-servers"));
-            kafkaConfig.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-            kafkaConfig.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-            kafkaConfig.put("group.id", config().getString("group-id"));
-            kafkaConfig.put("auto.offset.reset", "earliest");
-            kafkaConfig.put("enable.auto.commit", "false");
+            kafkaConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, config().getString("bootstrap-servers"));
+            kafkaConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+            kafkaConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+            kafkaConfig.put(ConsumerConfig.GROUP_ID_CONFIG, config().getString("group-id"));
+            kafkaConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+            kafkaConfig.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+            kafkaConfig.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, config().getString("security-protocol"));
+            kafkaConfig.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, config().getString("ssl-keystore-location"));
+            kafkaConfig.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, config().getString("ssl-keystore-password"));
+            kafkaConfig.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, config().getString("ssl-truststore-location"));
+            kafkaConfig.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, config().getString("ssl-truststore-password"));
             kafkaConsumer = KafkaConsumer.create(vertx, kafkaConfig);
             kafkaConsumer.handler(this::handleMessage);
             kafkaConsumer.subscribe(config().getString("topic-incident-assignment-event"));
